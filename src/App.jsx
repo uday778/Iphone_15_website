a92c8d1f50be836212dc943159564c0a082f7a03
uday kumar <uday33379@gmail.com>
2024-04-11 21:00:46 +0530

    initial

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import Hero from './components/Hero'
+import Highlights from './components/Highlights'
+import Navbar from './components/Navbar'
+
+
+const  App=()=> {
+
+
+  return (
+    <>
+      <main className='bg-black'>
+        <Navbar/>
+        <Hero/>
+        <Highlights/>
+      </main>
+    </>
+  )
+}
+
+export default App
